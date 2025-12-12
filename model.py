d72a7aeb194d4fe1a6f97ba1bd2c1b919873b671
25Inches <fake@email.com>
2025-12-12 15:07:09 +0100

    Use Rong's template

--- model.py
+++
@@ -1,6 +0,0 @@
-# This file contains the Model for retrieving weather from DMI and storing it in a pandas dataframe. It also treats the data, preparing it for viewing.
-import pandas as pd
-
-# Getting the data and creating a dataframe
-
-# Cleaning up data
