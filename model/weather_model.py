1e394f6039bde489a9053f05c64b78a4edb4130b
25Inches <fake@email.com>
2025-12-16 10:46:44 +0100

    Initial model

--- model/weather_model.py
+++ model/weather_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+from datetime import datetime, timedelta, timezone
 
 class WeatherModel:
     """
@@ -7,15 +8,29 @@ class WeatherModel:
     Returnerer data i et format, der er klar til plotting.
     """
 
-    def get_temperature_data(self):
-        # TODO: Udskift med ægte DMI API kald
-        # Eksempel på dummy-data:
-        data = [
-            {"name": "København", "lat": 55.6761, "lon": 12.5683, "temp": 6.4},
-            {"name": "Aarhus", "lat": 56.1629, "lon": 10.2039, "temp": 5.7},
-            {"name": "Odense", "lat": 55.4038, "lon": 10.4024, "temp": 6.0},
-        ]
-        return pd.DataFrame(data)
+    def __init__(self):
+        """Set up time variables"""
+        hours = 24
+        now = datetime.now(timezone.utc)
+        start = now - timedelta(hours=hours)
+        self.datetime_range = f"{start.isoformat()}/{now.isoformat()}"
+        
 
-    def fetch_from_dmi(self, endpoint):
-        pass
+    def get_temperature_data(self):
+        # Get data from DMI
+        response = self.fetch_from_dmi()
+        df = pd.json_normalize(response['features'])
+        #return pd.DataFrame(data)
+
+    def fetch_from_dmi(self):
+        # Url and params for fetching data
+        url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items"
+        params = {
+    "api-key": "75ab0b53-550a-4ede-b9ae-d1d9bc108f48",   # <- indsæt din egen nøgle
+    "stationId": "06081",       # station fx København
+    "parameterId": "temp_dry",  # parameter fx temperatur
+    "datetime": self.datetime_range,
+    "limit": 1000
+}
+        return requests.get(url, params=params).json()
+    
